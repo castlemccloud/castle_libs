0f85f244dc56eb0dce53af3c8bc7ab8aca3751c5
QuinMcCl <60714883+QuinMcCl@users.noreply.github.com>
2021-08-05 15:50:44 -0400

    Added Evaluate

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -1,68 +1,365 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <math.h>
-
 #include "matrix.h"
 
-int main(void)
-{
-	
-	srand(1234567890);
-	
-	long numFactors = 2;
-	long numPower = 2;
-	long numSamples = 2*pow(numPower,numFactors);
-	
-	matrix_t* b = make_matrix(1,pow(numPower,numFactors));
-	for(long i = 0; i < b->row; i++){
-		set_matrix(b, 0, i,  (rand() % 1000) / 50.0f - 10.0f);
+
+long modd(long A, long B) {
+	if (B == 0) return -1;
+	long k = A/B;
+	long rtn = A - k*B;
+	if (rtn < 0) rtn += B;
+	return rtn;
+}
+
+
+
+matrix_t * make_matrix(long col, long row) {
+	matrix_t * rtn = (matrix_t *) malloc(sizeof(matrix_t));
+	*rtn = (matrix_t){col, row, (double *)malloc(sizeof(double)*col*row)};
+	for(long i = 0; i < row*col; i++) {
+		rtn->data[i] = 0.0f;
 	}
-	printf("b\n");
-	print_matrix(b);
-	
-	matrix_t* x = make_matrix(numFactors, numSamples);
-	for(long i = 0; i < numSamples; i++){
-		for(long j = 0; j < numFactors; j++){
-			set_matrix(x, j, i,  (rand() % 1000) / 50.0f - 10.0f);
+	return rtn;
+}
+
+void destroy_matrix(matrix_t * M) {
+	if (M) {
+		free(M->data);
+		free(M);
+	}
+}
+
+void set_matrix(matrix_t * M, long col, long row, double value) {
+	if (M) {
+		col = modd(col, M->col);
+		row = modd(row, M->row);
+		M->data[col + row * M->col] = value;
+	}
+}
+double get_matrix(matrix_t * M, long col, long row) {
+	if (M) {
+		col = modd(col, M->col);
+		row = modd(row, M->row);
+		return M->data[col + row * M->col];
+	}
+	return 0.0;
+}
+
+matrix_t * super_matrix(matrix_t * M, long c_off, long r_off, long col, long row) {
+	if (M) {
+		matrix_t * rtn = make_matrix(col, row);
+		
+		for(long i = 0; i < row; i++) {
+			for(long j = 0; j < col; j++) {
+				
+				rtn->data[j + i*col] = M->data[modd(j + c_off, M->col) + modd(i + r_off, M->row)*M->col];
+				//set_matrix(rtn, j, i, get_matrix(M, c_off + j, r_off + i));
+			}
+		}
+		
+		return rtn;
+	}
+	return NULL;
+}
+
+void get_col_matrix(matrix_t * M, long col, double * data) {
+	if (M) {
+		col = modd(col, M->col);
+		
+		for(long i = 0, j = 0; i < M->row; i++, j += M->col) {
+			
+			data[i] = M->data[col + j];
+			//data[i] = get_matrix(M, col, i);
 		}
 	}
-	//printf("x\n");
-	//print_matrix(x);
-	
-	matrix_t* Y = make_matrix(1,numSamples);
-	for(long i = 0; i< numSamples; i++){
-		set_matrix(Y, 0, i,  evaluate_matrix(numPower, super_matrix(x, 0, i, numFactors, 1), b));
+}
+void get_row_matrix(matrix_t * M, long row, double * data) {
+	if (M) {
+		row = modd(row, M->row);
+		row *= M->col;
+		
+		for(long j = 0; j < M->col; j++) {
+			
+			data[j] = M->data[j + row];
+			//data[j] = get_matrix(M, j, row);
+		}
 	}
-	//printf("Y\n");
-	//print_matrix(Y);
+}
+
+void set_col_matrix(matrix_t * M, long col, double * data) {
+	if (M) {
+		col = modd(col, M->col);
+		
+		for(long i = 0; i < M->row; i++) {
+			
+			M->data[col + i*M->col] = data[i];
+			//set_matrix(M, col, i, data[i]);
+		}
+	}
+}
+
+void set_row_matrix(matrix_t * M, long row, double * data) {
+	if (M) {
+		row = modd(row, M->row);
+		row *= M->row;
+		
+		for(long j = 0; j < M->col; j++) {
+			
+			M->data[j + row] = data[j];
+			//set_matrix(M, j, row, data[j]);
+		}
+	}
+}
+
+double determinate(matrix_t * M) {
 	
-	matrix_t* X = design_matrix(numPower, x);
-	//printf("X\n");
-	//print_matrix(X);
-	matrix_t* XT = transpose_matrix(X);
-	//printf("XT\n");
-	//print_matrix(XT);
-	matrix_t* temp = mult_matrix(X,XT);
-	//printf("temp\n");
-	//print_matrix(temp);
-	matrix_t* Xi = inverse_matrix(temp);
-	//printf("Xi\n");
-	//print_matrix(Xi);
-	matrix_t* temp2 = mult_matrix(Y,XT);
-	//printf("temp2\n");
-	//print_matrix(temp2);
-	matrix_t* B = mult_matrix(temp2, Xi);
-	printf("B\n");
-	print_matrix(B);
-	
-	double TotalErr = 0;
-	for(long i = 0; i < numSamples; i++){
-		double val = get_matrix(Y, 0, i) - evaluate_matrix(numPower, super_matrix(x, 0, i, numFactors, 1), B);
-		TotalErr += val*val;
+	if (M && M->col == M->row) {
+		long l = M->col;
+		long m = l-1;
+		
+		if (l < 2) {
+			
+			return 0.0;
+			
+		} else if (l == 2) {
+			
+			return (get_matrix(M, 0, 0) * get_matrix(M, 1, 1)) - (get_matrix(M, 0, 1) * get_matrix(M, 1, 0));
+			
+			
+		} else {
+			
+			double sum = 0;
+			for(long i = 0; i < M->row*M->col; i += M->col) sum += M->data[i];
+			if (sum == 0.0) return 0.0;
+
+			matrix_t * temp = make_matrix(l, l);
+			for(long i = 0; i < l*l; i++) temp->data[i] = M->data[i];
+			
+			double F[l];
+			double Q[l];
+			
+			for(long i = 0; i < m; i++) {
+				
+				get_col_matrix(temp, i, F);
+				double sum = 0.0;
+				long a;
+				for(a = i+1; a < l; a++) sum += F[a];
+				if (a == 0.0) continue;
+				
+				get_row_matrix(temp, i, F);
+				double A = F[i];
+				
+				// A Zero Fix
+				if (A == 0.0) {
+					long a;
+					for(a == 0; a < l; a++) {
+						if (i != a && get_matrix(temp, i, a) != 0.0) {
+							break;
+						}
+					}
+					
+					if (a < l) {
+						get_row_matrix(temp, a, Q);
+						for(long b = 0; b < l; b++) {
+							F[i] += Q[i];
+						}
+						set_row_matrix(temp, i, F);
+						
+					} else {
+						printf("No Solution!?!?\n");
+						destroy_matrix(temp);
+						return 0.0;
+					}
+					
+				}
+				
+				
+				
+				for(long j = i+1; j < l; j++) {
+					long b = j*temp->col;
+					
+					for(long a = 0; a < l; a++) {
+						Q[a] = temp->data[a + b];
+					}
+					
+					double B = Q[i];
+					
+					if (B == 0.0) continue;
+					
+					double S = (B / A);
+					
+					for(long k = 0; k < l; k++) {
+						Q[k] = Q[k] - S*F[k];
+					}
+					
+					for(long a = 0; a < l; a++) {
+						temp->data[a + b] = Q[a];
+					}
+				}
+			}
+			
+			double product = 1.0;
+			
+			for(long i = 0; i < temp->row*temp->col; i += temp->col+1) {
+				
+				product *= temp->data[i];
+				
+			}
+			
+			destroy_matrix(temp);
+			
+			return product;
+			
+		}
 		
 	}
-	printf("Err %f\n",pow(TotalErr,0.5));
+	return 0.0;
+}
+
+matrix_t * transpose_matrix(matrix_t * M) {
+	if (M) {
+		
+		matrix_t * rtn = make_matrix(M->row, M->col);
+		for(long i = 0; i < rtn->row; i++) {
+			for(long j = 0; j < rtn->col; j++) {
+				
+				rtn->data[j + i*rtn->col] = M->data[i + j * M->col];
+				
+				//set_matrix(rtn, j, i, get_matrix(M, i, j));
+				
+			}
+		}
+		return rtn;
+	}
 	
-	return 0;
+	return NULL;
+}
+
+matrix_t * mult_matrix(matrix_t * A, matrix_t * B) {
+	
+	if (A && B && A->row == B->col) {
+		
+		long l = A->row;
+		
+		matrix_t * rtn = make_matrix(A->col, B->row);
+		
+		for(long i = 0; i < rtn->row; i++) {
+			for(long j = 0; j < rtn->col; j++) {
+				
+				double c[l]; get_col_matrix(A, j, c);
+				double r[l]; get_row_matrix(B, i, r);
+				
+				double sum = 0.0;
+				for(long k = 0; k < l; k++) {
+					sum += c[k]*r[k];
+				}
+				
+				set_matrix(rtn, j, i, sum);
+			}
+		}
+		
+		return rtn;
+	}
+	
+	return NULL;
+}
+
+matrix_t * design_matrix(long numPower, matrix_t * x) {
+	long numFactors = x->col;
+	long numSamples = x->row;
+	long XSize = pow(numPower,numFactors);
+	matrix_t* X = make_matrix(XSize, numSamples);
+	
+	for(long i = 0; i < numSamples; i++){
+		for(long j = 0; j < XSize; j++){
+			double prod = 1;
+			for(long k = 0; k < numFactors; k++){
+				prod *= pow(get_matrix(x,k,i), (j / (long)pow(numPower,k))%numPower);
+			}
+			set_matrix(X, j, i, prod);
+		}
+	}
+	return X;
+}
+
+double evaluate_matrix(long numPower, matrix_t * variables, matrix_t * design) {
+	double sum = 0;
+	for(long j = 0; j < design->row; j++){
+		double prod = get_matrix(design,0,j);
+		for(long k = 0; k < variables->col; k++){
+			prod *= pow(get_matrix(variables,k,0), (j / (long)pow(numPower,k))%numPower);
+		}
+		sum += prod;
+	}
+	
+	return sum;
+}
+
+matrix_t * inverse_matrix(matrix_t * M) {
+	
+	if (M && M->col == M->row) {
+		
+		long l = M->col;
+		long m = l-1;
+		
+		matrix_t * CoFactor = make_matrix(l, l);
+		for(long i = 0; i < l; i++) {
+			for(long j = 0; j < l; j++) {
+				
+				matrix_t * temp = super_matrix(M, j+1, i+1, m, m);
+				matrix_t * temp2 = super_matrix(temp, -j, -i, m, m);
+				
+				set_matrix(CoFactor, j, i, ((i+j)%2 ? -1.0 : 1.0) * determinate(temp2));
+				
+				destroy_matrix(temp2);
+				destroy_matrix(temp);
+				
+			}
+		}
+		
+		double deter = 0.0;
+		double r[l]; get_row_matrix(M, 0, r);
+		double c[l]; get_row_matrix(CoFactor, 0, c);
+		for(long i = 0; i < l; i++) {
+			deter += r[i]*c[i];
+		}
+		
+		if (deter == 0.0) {
+			printf("Singularity!\n");
+			destroy_matrix(CoFactor);
+			return NULL;
+		}
+		
+		double ideter = 1.0 / deter;
+		
+		
+		matrix_t * rtn = make_matrix(l, l);
+		for(long i = 0; i < l; i++) {
+			for(long j = 0; j < l; j++) {
+				
+				set_matrix(rtn, j, i, get_matrix(CoFactor, i, j) * ideter);
+				
+			}
+		}
+		
+		destroy_matrix(CoFactor);
+		
+		
+		return rtn;
+	}
+	
+	return NULL;
+}
+
+void print_matrix(matrix_t * M) {
+	if (M) {
+		
+		for(long i = 0; i < M->row; i++) {
+			long l = M->col - 1;
+			printf("[");
+			for(long j = 0; j < M->col; j++) {
+				printf("% 6.3f%c", get_matrix(M, j, i), (j == l ? ']' : '\t'));
+			}
+			printf("\n");
+		}
+		
+	}
 }
