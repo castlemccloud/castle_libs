5edd2cbc9b1d1e87ead2f9b3d87e15ee64f550b6
QuinMcCl <60714883+QuinMcCl@users.noreply.github.com>
2021-08-06 12:10:33 -0400

    Update matrix.c
    https://www.mpfr.org/mpfr-current/mpfr.htm

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -13,28 +13,34 @@ long modd(long A, long B) {
 
 matrix_t * make_matrix(long col, long row) {
 	matrix_t * rtn = (matrix_t *) malloc(sizeof(matrix_t));
-	*rtn = (matrix_t){col, row, (double *)malloc(sizeof(double)*col*row)};
+	*rtn = (matrix_t){col, row, (mpfr_t *)malloc(sizeof(mpfr_t)*col*row)};
 	for(long i = 0; i < row*col; i++) {
-		rtn->data[i] = 0.0f;
+		mpfr_init2 (rtn->data[i], MPFR_PRECISION);
+		mpfr_set_d (rtn->data[i], 0.0, MPFR_RNDD);
+		//rtn->data[i] = 0.0f;
 	}
 	return rtn;
 }
 
 void destroy_matrix(matrix_t * M) {
 	if (M) {
+		for(long i = 0; i < row*col; i++) {
+			mpfr_clear (M->data[i]);
+		}
 		free(M->data);
 		free(M);
 	}
 }
 
-void set_matrix(matrix_t * M, long col, long row, double value) {
+void set_matrix(matrix_t * M, long col, long row, mpfr_t value) {
 	if (M) {
 		col = modd(col, M->col);
 		row = modd(row, M->row);
-		M->data[col + row * M->col] = value;
+		mpfr_set (M->data[col + row * M->col], value, MPFR_RNDD);
+		//M->data[col + row * M->col] = value;
 	}
 }
-double get_matrix(matrix_t * M, long col, long row) {
+mpfr_t get_matrix(matrix_t * M, long col, long row) {
 	if (M) {
 		col = modd(col, M->col);
 		row = modd(row, M->row);
@@ -50,8 +56,8 @@ matrix_t * super_matrix(matrix_t * M, long c_off, long r_off, long col, long row
 		for(long i = 0; i < row; i++) {
 			for(long j = 0; j < col; j++) {
 				
-				rtn->data[j + i*col] = M->data[modd(j + c_off, M->col) + modd(i + r_off, M->row)*M->col];
-				//set_matrix(rtn, j, i, get_matrix(M, c_off + j, r_off + i));
+				//rtn->data[j + i*col] = M->data[modd(j + c_off, M->col) + modd(i + r_off, M->row)*M->col];
+				set_matrix(rtn, j, i, get_matrix(M, c_off + j, r_off + i));
 			}
 		}
 		
@@ -60,56 +66,59 @@ matrix_t * super_matrix(matrix_t * M, long c_off, long r_off, long col, long row
 	return NULL;
 }
 
-void get_col_matrix(matrix_t * M, long col, double * data) {
+void get_col_matrix(matrix_t * M, long col, mpfr_t  * data) {
 	if (M) {
 		col = modd(col, M->col);
 		
 		for(long i = 0, j = 0; i < M->row; i++, j += M->col) {
 			
-			data[i] = M->data[col + j];
+			//data[i] = M->data[col + j];
 			//data[i] = get_matrix(M, col, i);
+			mpfr_set (data[i], get_matrix(M, col, i), MPFR_RNDD);
 		}
 	}
 }
-void get_row_matrix(matrix_t * M, long row, double * data) {
+void get_row_matrix(matrix_t * M, long row, mpfr_t  * data) {
 	if (M) {
 		row = modd(row, M->row);
 		row *= M->col;
 		
 		for(long j = 0; j < M->col; j++) {
 			
-			data[j] = M->data[j + row];
+			//data[j] = M->data[j + row];
 			//data[j] = get_matrix(M, j, row);
+			mpfr_set (data[i], get_matrix(M, j, row), MPFR_RNDD);
 		}
 	}
 }
 
-void set_col_matrix(matrix_t * M, long col, double * data) {
+void set_col_matrix(matrix_t * M, long col, mpfr_t  * data) {
 	if (M) {
 		col = modd(col, M->col);
 		
 		for(long i = 0; i < M->row; i++) {
 			
-			M->data[col + i*M->col] = data[i];
-			//set_matrix(M, col, i, data[i]);
+			//M->data[col + i*M->col] = data[i];
+			set_matrix(M, col, i, data[i]);
+			
 		}
 	}
 }
 
-void set_row_matrix(matrix_t * M, long row, double * data) {
+void set_row_matrix(matrix_t * M, long row, mpfr_t  * data) {
 	if (M) {
 		row = modd(row, M->row);
 		row *= M->row;
 		
 		for(long j = 0; j < M->col; j++) {
 			
-			M->data[j + row] = data[j];
-			//set_matrix(M, j, row, data[j]);
+			//M->data[j + row] = data[j];
+			set_matrix(M, j, row, data[j]);
 		}
 	}
 }
 
-double determinate(matrix_t * M) {
+mpfr_t determinate(matrix_t * M) {
 	
 	if (M && M->col == M->row) {
 		long l = M->col;
@@ -117,41 +126,60 @@ double determinate(matrix_t * M) {
 		
 		if (l < 2) {
 			
-			return 0.0;
+			return get_matrix(M, 0, 0);
 			
 		} else if (l == 2) {
 			
-			return (get_matrix(M, 0, 0) * get_matrix(M, 1, 1)) - (get_matrix(M, 0, 1) * get_matrix(M, 1, 0));
+			//return (get_matrix(M, 0, 0) * get_matrix(M, 1, 1)) - (get_matrix(M, 0, 1) * get_matrix(M, 1, 0));
 			
+			mpfr_t a, b;
+			mpfr_inits (a, b);
+			mpfr_set (a, get_matrix(M, 0, 0), MPFR_RNDD);
+			mpfr_mul (a, a, get_matrix(M, 1, 1), MPFR_RNDU);
+			
+			mpfr_set (b, get_matrix(M, 0, 1), MPFR_RNDD);
+			mpfr_mul (b, b, get_matrix(M, 1, 0), MPFR_RNDU);
+			
+			mpfr_sub (a, a, b, MPFR_RNDU);
+			mpfr_clear (b);
+			
+			return a;
 			
 		} else {
 			
-			double sum = 0;
-			for(long i = 0; i < M->row*M->col; i += M->col) sum += M->data[i];
-			if (sum == 0.0) return 0.0;
+			//double sum = 0;
+			mpfr_t sum;
+			mpfr_init_set_ui (sum, 0,MPFR_RNDD);
+			for(long i = 0; i < M->row*M->col; i += M->col) {
+				//sum += M->data[i];
+				mpfr_add (sum, sum, M->data[i], MPFR_RNDD);
+			}
+			//if (sum == 0.0) return 0.0;
+			if (! mpfr_cmp_ui(sum,0)) return sum;
+			mpfr_clear (sum);
 
-			matrix_t * temp = make_matrix(l, l);
-			for(long i = 0; i < l*l; i++) temp->data[i] = M->data[i];
+			//matrix_t * temp = make_matrix(l, l);
+			//for(long i = 0; i < l*l; i++) temp->data[i] = M->data[i];
 			
-			double F[l];
-			double Q[l];
+			matrix_t * temp = super_matrix(M,0,0,l,l);
+			
+			mpfr_t F[l];
+			mpfr_t Q[l];
 			
 			for(long i = 0; i < m; i++) {
-				
-				get_col_matrix(temp, i, F);
-				double sum = 0.0;
-				long a;
-				for(a = i+1; a < l; a++) sum += F[a];
-				if (a == 0.0) continue;
-				
+					
 				get_row_matrix(temp, i, F);
-				double A = F[i];
+				//double A = F[i];
+				mpfr_t A;
+				mpfr_init_set (A, F[i],MPFR_RNDD);
 				
 				// A Zero Fix
-				if (A == 0.0) {
+				//if (A == 0.0) {
+				if (! mpfr_cmp_ui(A,0)){
 					long a;
-					for(a == 0; a < l; a++) {
-						if (i != a && get_matrix(temp, i, a) != 0.0) {
+					for(a = 0; a < l; a++) {
+						//if (i != a && get_matrix(temp, i, a) != 0.0) {
+						if (i != a && mpfr_cmp_ui(get_matrix(temp, i, a),0) ) {
 							break;
 						}
 					}
@@ -159,32 +187,38 @@ double determinate(matrix_t * M) {
 					if (a < l) {
 						get_row_matrix(temp, a, Q);
 						for(long b = 0; b < l; b++) {
-							F[i] += Q[i];
+							//F[i] += Q[i];
+							mpfr_add (F[i], F[i], Q[i], MPFR_RNDD);
 						}
 						set_row_matrix(temp, i, F);
 						
 					} else {
 						printf("No Solution!?!?\n");
 						destroy_matrix(temp);
-						return 0.0;
+						return A;
 					}
 					
 				}
-				
-				
-				
+/*
+*--------------------------------------------------------------------------------STOPPED_HERE----------------------------
+*/
 				for(long j = i+1; j < l; j++) {
-					long b = j*temp->col;
+					//long b = j*temp->col;
 					
-					for(long a = 0; a < l; a++) {
-						Q[a] = temp->data[a + b];
-					}
+					//for(long a = 0; a < l; a++) {
+						//Q[a] = temp->data[a + b];
+					//}
+					get_row_matrix(temp, j, Q);
 					
-					double B = Q[i];
+					mpfr_t B = Q[i];
 					
-					if (B == 0.0) continue;
+					//if (B == 0.0) continue;
+					if(! mpfr_cmp_ui(Q[i],0))continue
 					
 					double S = (B / A);
+					mpfr_t S;
+					mpfr_init(S);
+					mpfr_div(S, B, A, MPFR_RNDD);
 					
 					for(long k = 0; k < l; k++) {
 						Q[k] = Q[k] - S*F[k];
@@ -196,8 +230,7 @@ double determinate(matrix_t * M) {
 				}
 			}
 			
-			double product = 1.0;
-			
+			long double product = 1.0;
 			for(long i = 0; i < temp->row*temp->col; i += temp->col+1) {
 				
 				product *= temp->data[i];
@@ -205,6 +238,7 @@ double determinate(matrix_t * M) {
 			}
 			
 			destroy_matrix(temp);
+			
 			
 			return product;
 			
@@ -311,7 +345,6 @@ matrix_t * inverse_matrix(matrix_t * M) {
 				
 				destroy_matrix(temp2);
 				destroy_matrix(temp);
-				
 			}
 		}
 		
@@ -321,7 +354,6 @@ matrix_t * inverse_matrix(matrix_t * M) {
 		for(long i = 0; i < l; i++) {
 			deter += r[i]*c[i];
 		}
-		
 		if (deter == 0.0) {
 			printf("Singularity!\n");
 			destroy_matrix(CoFactor);
@@ -329,7 +361,6 @@ matrix_t * inverse_matrix(matrix_t * M) {
 		}
 		
 		double ideter = 1.0 / deter;
-		
 		
 		matrix_t * rtn = make_matrix(l, l);
 		for(long i = 0; i < l; i++) {
@@ -356,7 +387,7 @@ void print_matrix(matrix_t * M) {
 			long l = M->col - 1;
 			printf("[");
 			for(long j = 0; j < M->col; j++) {
-				printf("% 6.3f%c", get_matrix(M, j, i), (j == l ? ']' : '\t'));
+				printf("%f%c", get_matrix(M, j, i), (j == l ? ']' : '\t'));
 			}
 			printf("\n");
 		}
