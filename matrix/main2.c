fcc341a56c067bbf5427f114026d5f2b8500ffa9
QuinMcCl <60714883+QuinMcCl@users.noreply.github.com>
2021-08-06 17:53:34 -0400

    Update main2.c

--- matrix/main2.c
+++ matrix/main2.c
@@ -10,13 +10,14 @@ int main(void)
 	
 	srand(1234567890);
 	
-	long numFactors = 2;
-	long numPower = 2;
+	long numFactors = 4;
+	long numPower = 4;
 	long numSamples = 2*pow(numPower,numFactors);
-	
+	mpc_t T; mpc_init2(T, PRECISION);
 	matrix_t* b = make_matrix(1,pow(numPower,numFactors));
 	for(long i = 0; i < b->row; i++){
-		set_matrix(b, 0, i,  (rand() % 1000) / 50.0f - 10.0f);
+		mpc_set_d(T, (((double)(rand() % 1000) / 500.0) - 1.0), MPC_RNDDD);
+		set_matrix(b, 0, i,  T);
 	}
 	printf("b\n");
 	print_matrix(b);
@@ -24,18 +25,20 @@ int main(void)
 	matrix_t* x = make_matrix(numFactors, numSamples);
 	for(long i = 0; i < numSamples; i++){
 		for(long j = 0; j < numFactors; j++){
-			set_matrix(x, j, i,  (rand() % 1000) / 50.0f - 10.0f);
+			mpc_set_d(T, (((double)(rand() % 1000) / 500.0) - 1.0), MPC_RNDDD);
+			set_matrix(x, j, i,  T);
 		}
 	}
-	//printf("x\n");
-	//print_matrix(x);
+	printf("x\n");
+	print_matrix(x);
 	
 	matrix_t* Y = make_matrix(1,numSamples);
 	for(long i = 0; i< numSamples; i++){
-		set_matrix(Y, 0, i,  evaluate_matrix(numPower, super_matrix(x, 0, i, numFactors, 1), b));
+		evaluate_matrix(numPower, super_matrix(x, 0, i, numFactors, 1), b, T);
+		set_matrix(Y, 0, i, T);
 	}
-	//printf("Y\n");
-	//print_matrix(Y);
+	printf("Y\n");
+	print_matrix(Y);
 	
 	matrix_t* X = design_matrix(numPower, x);
 	//printf("X\n");
@@ -56,13 +59,24 @@ int main(void)
 	printf("B\n");
 	print_matrix(B);
 	
-	double TotalErr = 0;
+	mpc_t TotalErr; mpc_init2(TotalErr, PRECISION);
+	mpc_set_si(TotalErr, 0, MPC_RNDDD);
+	mpc_t val; mpc_init2(val, PRECISION);
 	for(long i = 0; i < numSamples; i++){
-		double val = get_matrix(Y, 0, i) - evaluate_matrix(numPower, super_matrix(x, 0, i, numFactors, 1), B);
-		TotalErr += val*val;
+		evaluate_matrix(numPower, super_matrix(x, 0, i, numFactors, 1), B, T);
+		get_matrix(Y, 0, i, val);
+		mpc_sub(val,val,T,MPC_RNDDD);
+		mpc_mul(val,val,val,MPC_RNDDD);
+		mpc_add(TotalErr,TotalErr,val,MPC_RNDDD);
 		
 	}
-	printf("Err %f\n",pow(TotalErr,0.5));
+	mpc_pow_d(TotalErr,TotalErr,0.5,MPC_RNDDD);
+	char * str = mpc_get_str(10, 6, TotalErr, MPC_RNDDD);
+	printf("Err %14s\n",str);
+	mpc_free_str(str);
+	mpc_clear(T);
+	mpc_clear(TotalErr);
+	mpc_clear(val);
 	
 	return 0;
 }
